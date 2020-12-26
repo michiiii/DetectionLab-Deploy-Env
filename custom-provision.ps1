d54b3a613af3405caa8ee2e968f745ae0efaadce
Mc Ridda <info@michi-ritter.de>
2020-12-26 18:11:48 +0100

    Create custom-provision.ps1

---
+++ custom-provision.ps1
@@ -0,0 +1,8 @@
+Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Setting timezone to W. Europe Standard Time..."
+c:\windows\system32\tzutil.exe /s "W. Europe Standard Time"
+
+Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Setting keyboard language to German..."
+Set-WinUserLanguageList -LanguageList de-DE -Force
+
+Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Disable NLA..."
+(Get-WmiObject -class "Win32_TSGeneralSetting" -Namespace root\cimv2\terminalservices -Filter "TerminalName='RDP-tcp'").SetUserAuthenticationRequired(0)
